c8069aec43da262ca7ff266fbdb93bc051fcb468
Mehdi Hmidi <mh223vk@student.lnu.se>
2018-10-01 13:24:08 +0200

    Initial commit

---
+++ src/MyBST.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Mehdi on 01/10/2018 for the MyIntegerBST project.
+ */
+public class MyBST {
+    public static void main(String[] args){
+
+    }
+
+    public MyBST(){
+
+    }
+
+    public void insert(int value){
+
+    }
+
+    public int mostSimilarValue(int value){
+
+        return -99;
+    }
+
+}
